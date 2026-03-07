32bd06b0505b37244b6786af59c9df7eac4d2686
Ahmed <ahmedmessad345@gmail.com>
2026-03-06 20:36:00 -0800

    Fix missing file

---
+++ src/HIS.Application/DTOs/DoctorSchedule/DoctorSchedulesListDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIS.Application.DTOs.DoctorSchedule
+{
+    public class DoctorSchedulesListDto
+    {
+        public TimeOnly StartTime { get; set; }
+
+        public TimeOnly EndTime { get; set; }
+
+        public float SlotDurationMinutes { get; set; }
+        public Guid DayOfWeekId { get; set; }
+    }
+}
